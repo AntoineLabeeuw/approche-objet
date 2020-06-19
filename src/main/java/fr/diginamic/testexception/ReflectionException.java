adb89bdf3718811fab6a3ff1eb44d886d80a7dd7
Antoine LABEEUW <antoine.labeeuw@gmail.com>
2020-06-19 17:52:57 +0200

    add files for exceptions

---
+++ src/main/java/fr/diginamic/testexception/ReflectionException.java
@@ -0,0 +1,12 @@
+package fr.diginamic.testexception;
+
+public class ReflectionException extends Exception {
+
+	/** Constructeur
+	 * @param message
+	 */
+	public ReflectionException(String message) {
+		super(message);
+	}
+	
+}
---
+++ src/main/java/fr/diginamic/testexception/TestReflectionUtils.java
@@ -0,0 +1,23 @@
+package fr.diginamic.testexception;
+
+import tri.Pays;
+
+public class TestReflectionUtils {
+
+	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException, ReflectionException{
+		Pays paysFrance = new Pays("USA", 332_639_000, 59_495);
+		ReflectionUtils.afficherAttributs(paysFrance);
+		// essai avec null
+		Pays paysVide = null;
+		try {
+			ReflectionUtils.afficherAttributs(paysVide);
+		} catch(ReflectionException e) {
+			System.err.println(e.getMessage());
+		}
+		catch( IllegalArgumentException | IllegalAccessException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
